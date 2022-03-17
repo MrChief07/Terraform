7415b16b92c16a8dc6a4b149f16f8897aa78e779
MrChief07 <101746598+MrChief07@users.noreply.github.com>
2022-03-17 15:16:31 +0530

    main.tf
    we are trying to add multiple private endpoints to one container registries

---
+++ main.tf
@@ -0,0 +1,111 @@
+
+#we are trying to add multiple private endpoints to one container registries
+
+terraform {
+
+  required_version = ">= 0.12"
+
+} 
+
+provider "azurerm" {
+
+  version = ">=2.20"
+
+  features {} 
+
+}
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_resource_group" "rgrp" {
+
+  name = "Test_rg"
+
+}
+
+data "azurerm_container_registry" "main" {
+
+  name                          = "npdcontainerregistry"
+
+  resource_group_name           = data.azurerm_resource_group.rgrp.name
+
+}
+
+data "azurerm_virtual_network" "vnet01" {
+
+  name                = "securitytesting"
+
+  resource_group_name = data.azurerm_resource_group.rgrp.name
+
+}
+
+data "azurerm_subnet" "subnets" {
+
+  for_each             = var.subnets
+
+  name                 = each.value.name
+
+  resource_group_name                            = data.azurerm_resource_group.rgrp.name
+  virtual_network_name                           = data.azurerm_virtual_network.vnet01.name
+
+}
+
+/*
+resource "azurerm_subnet" "updatesubnet" {
+for_each             = var.subnets
+  name                 = each.value.name
+  resource_group_name                            = data.azurerm_resource_group.rgrp.name
+  virtual_network_name                           = data.azurerm_virtual_network.vnet01.name
+  address_prefixes                               = each.value.addressprefixes
+  enforce_private_link_endpoint_network_policies = true
+}
+
+*/
+
+resource "azurerm_private_endpoint" "pep1" {
+
+  for_each             =var.subnets
+
+  name                = each.value.privateendpoint
+
+  location            = data.azurerm_resource_group.rgrp.location
+
+  resource_group_name = data.azurerm_resource_group.rgrp.name
+
+  subnet_id           = data.azurerm_subnet.subnets[each.key].id 
+
+  private_dns_zone_group {
+    name                 = "npdcontainerregistry-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dnszone1[each.key].id]
+    
+  }
+
+  private_service_connection {
+
+    name                           = each.value.serviceconnectname
+
+    is_manual_connection           = false
+
+    private_connection_resource_id = data.azurerm_container_registry.main.id
+
+    subresource_names              = ["registry"]
+
+   }
+
+ }
+
+
+resource "azurerm_private_dns_zone" "dnszone1" {
+  for_each             =var.subnets
+  name                = each.value.dnsname
+  resource_group_name = data.azurerm_resource_group.rgrp.name
+  
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vent-link1" {
+  for_each              = var.subnets
+  name                  = each.value.vnetlinkname
+  resource_group_name   = data.azurerm_resource_group.rgrp.name
+  private_dns_zone_name = azurerm_private_dns_zone.dnszone1[each.key].name
+  virtual_network_id    = data.azurerm_virtual_network.vnet01.id
+}
