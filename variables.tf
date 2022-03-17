75e20cc3a2a8b4b3c3bc00072a865a05d39916a1
MrChief07 <101746598+MrChief07@users.noreply.github.com>
2022-03-17 15:18:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+
+
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+  default     = "*****"
+}
+
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
+  default     = "****"
+}
+
+variable "subnets" {}
+
+
